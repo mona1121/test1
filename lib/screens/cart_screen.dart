caf74aef53ecf18b7a7e9fed6723e43999304486
TaifFaisal9 <taiffiasal9@gmail.com>
2024-10-29 19:23:08 +0300

    Add invoice and payment screens; modify cart screen

--- lib/screens/cart_screen.dart
+++ lib/screens/cart_screen.dart
@@ -1,25 +1,3 @@
-/*
-import 'package:flutter/material.dart';
-
-class CartScreen extends StatelessWidget {
-  const CartScreen({super.key}); // Use const constructor
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Cart'),
-      ),
-      body: const Center(
-        child: Text('Your cart items will be displayed here.'),
-      ),
-    );
-  }
-}
-
- */
-
-// cart_screen.dart
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
@@ -28,7 +6,7 @@ class CartItem {
   final String id;
   final String name;
   final double price;
-  final int quantity;
+  int quantity;
   final String description;
   final String image;
 
@@ -41,7 +19,6 @@ class CartItem {
     required this.image,
   });
 
-  // Convert CartItem from Firestore snapshot
   factory CartItem.fromFirestore(DocumentSnapshot<Map<String, dynamic>> doc) {
     final data = doc.data()!;
     return CartItem(
@@ -54,31 +31,61 @@ class CartItem {
     );
   }
 
-  // Convert CartItem to JSON (if needed for other operations)
   Map<String, dynamic> toJson() => {
-    'id': id,
-    'name': name,
-    'price': price,
-    'quantity': quantity,
-    'description': description,
-    'image': image,
-  };
+        'name': name,
+        'price': price,
+        'quantity': quantity,
+        'description': description,
+        'image': image,
+      };
 }
 
-// CartScreen class that displays the cart items from Firestore.
-class CartScreen extends StatelessWidget {
+class CartScreen extends StatefulWidget {
   const CartScreen({super.key});
+
+  @override
+  _CartScreenState createState() => _CartScreenState();
+}
+
+class _CartScreenState extends State<CartScreen> {
+  double _totalPrice = 0.0;
+
+  void _updateQuantity(CartItem item, int change) {
+    final newQuantity = item.quantity + change;
+    if (newQuantity > 0) {
+      FirebaseFirestore.instance.collection('cartItems').doc(item.id).update({
+        'quantity': newQuantity,
+      });
+    } else {
+      FirebaseFirestore.instance.collection('cartItems').doc(item.id).delete();
+    }
+  }
+
+  double _calculateTotalPrice(List<CartItem> items) {
+    return items.fold(0.0, (sum, item) => sum + (item.price * item.quantity));
+  }
+
+  void _onPaymentButtonPressed() {
+    // Handle payment logic here
+    print("Payment button pressed!");
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Cart'),
+        title: const Text(
+          'Cart',
+          style: TextStyle(
+            fontFamily: 'LeagueSpartan',
+            fontSize: 24.0, // Adjusted font size for AppBar
+            fontWeight: FontWeight.w600,
+            color: Colors.black,
+          ),
+        ),
       ),
       body: StreamBuilder<QuerySnapshot<Map<String, dynamic>>>(
-        stream: FirebaseFirestore.instance
-            .collection('cartItems') // The Firestore collection for cart items
-            .snapshots(), // Stream of real-time updates from Firestore
+        stream: FirebaseFirestore.instance.collection('cartItems').snapshots(),
         builder: (context, snapshot) {
           if (!snapshot.hasData || snapshot.data!.docs.isEmpty) {
             return const Center(
@@ -86,52 +93,127 @@ class CartScreen extends StatelessWidget {
             );
           }
 
-          // Convert Firestore documents to CartItem objects
           final items = snapshot.data!.docs
               .map((doc) => CartItem.fromFirestore(doc))
               .toList();
 
-          return ListView.builder(
-            itemCount: items.length,
-            itemBuilder: (context, index) {
-              final item = items[index];
-              return ListTile(
-                leading: Image.network(item.image), // Display product thumbnail
-                title: Text(item.name),
-                subtitle: Text(
-                    '${item.description}\nPrice: \$${item.price} x ${item.quantity}'),
-                trailing: IconButton(
-                  icon: const Icon(Icons.delete),
-                  onPressed: () {
-                    // Remove item from Firestore
-                    FirebaseFirestore.instance
-                        .collection('cartItems')
-                        .doc(item.id)
-                        .delete();
+          _totalPrice = _calculateTotalPrice(items);
+
+          return Column(
+            children: [
+              Expanded(
+                child: ListView.builder(
+                  itemCount: items.length,
+                  itemBuilder: (context, index) {
+                    final item = items[index];
+                    return ListTile(
+                      contentPadding: const EdgeInsets.all(16.0),
+                      leading: Image.network(
+                        item.image,
+                        width: 100,
+                        height: 100,
+                        fit: BoxFit.contain,
+                      ),
+                      title: Text(
+                        item.name,
+                        style: const TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      subtitle: Padding(
+                        padding: const EdgeInsets.only(top: 8.0),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              'Price: \$${item.price.toStringAsFixed(2)}',
+                              style: const TextStyle(
+                                fontSize: 16,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            const SizedBox(height: 4),
+                            Row(
+                              children: [
+                                IconButton(
+                                  icon: const Icon(Icons.remove),
+                                  onPressed: () {
+                                    _updateQuantity(item, -1);
+                                  },
+                                ),
+                                Text(
+                                  '${item.quantity}',
+                                  style: const TextStyle(
+                                    fontSize: 18,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                                IconButton(
+                                  icon: const Icon(Icons.add),
+                                  onPressed: () {
+                                    _updateQuantity(item, 1);
+                                  },
+                                ),
+                              ],
+                            ),
+                          ],
+                        ),
+                      ),
+                      trailing: IconButton(
+                        icon: const Icon(Icons.delete),
+                        onPressed: () {
+                          FirebaseFirestore.instance
+                              .collection('cartItems')
+                              .doc(item.id)
+                              .delete();
+                        },
+                      ),
+                    );
                   },
                 ),
-              );
-            },
+              ),
+              Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      'Total Price: \$${_totalPrice.toStringAsFixed(2)}',
+                      style: const TextStyle(
+                        fontSize: 22,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    ElevatedButton(
+                      onPressed: _onPaymentButtonPressed,
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.black,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(20),
+                        ),
+                        minimumSize:
+                            const Size(150, 50), // Set the minimum size
+                        padding: const EdgeInsets.symmetric(
+                          horizontal: 32,
+                          vertical: 16,
+                        ),
+                      ),
+                      child: const Text(
+                        'Pay Now',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
           );
         },
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          // Example of adding a new item to Firestore
-          final newItem = CartItem(
-            id: DateTime.now().toString(),
-            name: 'Sample Product',
-            price: 15.0,
-            quantity: 1,
-            description: 'A sample product description',
-            image:
-            'https://via.placeholder.com/150', // Placeholder thumbnail URL
-          );
-
-          // Add new item to Firestore
-          FirebaseFirestore.instance.collection('cartItems').add(newItem.toJson());
-        },
-        child: const Icon(Icons.add),
       ),
     );
   }
